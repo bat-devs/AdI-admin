48159be91e26428a212298370bd32abd96b6aead
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2021-01-17 22:11:53 +0100

    Init and create home page UI (#3) (#6)
    * feat: init and create home page
    
    * refactor: rename README (1).md file name to README.md
    
    * feat: change logo img
    
    Co-authored-by: Atila <atila@github.com>
    Co-authored-by: git-marcopitra <marcomorete73@hotmail.com>
    
    Co-authored-by: atilasousa <52140947+atilasousa@users.noreply.github.com>
    Co-authored-by: Atila <atila@github.com>

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <base-header style="background-color:#DB8D07" class="pb-6 pb-8 pt-5 pt-md-8">
+      <!-- Card stats -->
+    </base-header>
+
+    
+  </div>
+</template>
+<script>
+// Charts
+import * as chartConfigs from "@/components/Charts/config";
+
+export default {
+  components: {},
+  data() {
+    return {
+      bigLineChart: {
+        allData: [
+          [0, 20, 10, 30, 15, 40, 20, 60, 60],
+          [0, 20, 5, 25, 10, 30, 15, 40, 40],
+        ],
+        activeIndex: 0,
+        chartData: {
+          datasets: [],
+          labels: [],
+        },
+        extraOptions: chartConfigs.blueChartOptions,
+      },
+      redBarChart: {
+        chartData: {
+          labels: ["Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+          datasets: [
+            {
+              label: "Sales",
+              data: [25, 20, 30, 22, 17, 29],
+            },
+          ],
+        },
+      },
+    };
+  },
+  methods: {
+    initBigChart(index) {
+      let chartData = {
+        datasets: [
+          {
+            label: "Performance",
+            data: this.bigLineChart.allData[index],
+          },
+        ],
+        labels: ["May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      };
+      this.bigLineChart.chartData = chartData;
+      this.bigLineChart.activeIndex = index;
+    },
+  },
+  mounted() {
+    this.initBigChart(0);
+  },
+};
+</script>
+<style></style>
