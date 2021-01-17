29d7fcd68cf48f38c543460d2d29a18ec3b459c8
atilasousa <52140947+atilasousa@users.noreply.github.com>
2021-01-17 22:07:56 +0100

    Init and create home page UI (#3)
    * feat: init and create home page
    
    * refactor: rename README (1).md file name to README.md
    
    * feat: change logo img
    
    Co-authored-by: Atila <atila@github.com>
    Co-authored-by: git-marcopitra <marcomorete73@hotmail.com>

---
+++ src/router.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import DashboardLayout from '@/layout/DashboardLayout'
+import AuthLayout from '@/layout/AuthLayout'
+Vue.use(Router)
+
+export default new Router({
+  linkExactActiveClass: 'active',
+  routes: [
+    {
+      path: '/',
+      redirect: 'home',
+      component: DashboardLayout,
+      children: [
+        {
+          path: '/home',
+          name: 'Home',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "demo" */ './views/Dashboard.vue')
+        },
+        {
+          path: '/adicionar-noticia',
+          name: 'AdicionarNoticia',
+          component: () => import(/* webpackChunkName: "demo" */ './views/AdicionarNoticia.vue')
+        },
+        {
+          path: '/profile',
+          name: 'profile',
+          component: () => import(/* webpackChunkName: "demo" */ './views/UserProfile.vue')
+        },
+        {
+          path: '/listar-noticias',
+          name: 'ListarNoticias',
+          component: () => import(/* webpackChunkName: "demo" */ './views/ListarNoticias.vue')
+        },
+      ]
+    },
+    {
+      path: '/',
+      redirect: 'login',
+      component: AuthLayout,
+      children: [
+        {
+          path: '/login',
+          name: 'login',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Login.vue')
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Register.vue')
+        }
+      ]
+    }
+  ]
+})
